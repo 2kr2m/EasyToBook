2ac2a22c9f3d3ea74c25a16e1b3c848010169add
2kr2m <ak1ram@outlook.com>
2023-12-08 17:24:42 +0100

    add crud functionnalities to Villa model

--- EasyToBook.WebApp/Controllers/VillaController.cs
+++ EasyToBook.WebApp/Controllers/VillaController.cs
@@ -37,5 +37,55 @@ namespace EasyToBook.WebApp.Controllers
             }
             return View();
         }
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? villa = _context.Villas.FirstOrDefault(x => x.Id == villaId);
+            if(villa == null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa editedVilla)
+        {
+            if (editedVilla.Name == editedVilla.Description)
+            {
+                ModelState.AddModelError("", "Name and Description cannot be the same");
+            }
+            if (ModelState.IsValid & editedVilla.Id>0)
+            {
+                _context.Villas.Update(editedVilla);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? villa = _context.Villas.FirstOrDefault(x => x.Id == villaId);
+            if (villa is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa deletedVilla)
+        {
+            Villa? check = _context.Villas.FirstOrDefault(u => u.Id == deletedVilla.Id);
+            if (check is not null)
+            {
+                _context.Villas.Remove(check);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
     }
 }
